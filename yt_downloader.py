0d142bb70b8a1d134a55ec3a16010dbd550dffa4
Joshua Mathew <umjoshua@gmail.com>
2021-04-09 09:14:51 +0530

    yt downloader

---
+++ yt_downloader.py
@@ -0,0 +1,19 @@
+	# From terminal, pip install pytube
+from pytube import YouTube
+
+print("***Youtube Downloader***\n")
+
+url=YouTube(input("Enter URL: "))
+format = url.streams.filter(progressive=True).all()
+
+index = list(enumerate(format))
+
+for i in index:
+    print(i)
+
+opt = int(input("\nEnter video format (index): "))
+vid = format[opt]
+print("\nDownloading....\n")
+vid.download()
+
+print("***Download Successfull***\n")
